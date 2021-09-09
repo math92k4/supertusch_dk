f4c2c240308c4eca1166c929ccb7502d92f2b54f
Mathias <math92k4@stud.kea.dk>
2021-09-09 11:34:08 +0200

    mobile init

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ import { randomizeContent } from './randomize_content.js'
 window.addEventListener('DOMContentLoaded', init)
 
 function init() {
+  document.querySelector("h3").addEventListener("click", () => {
+    document.querySelector("footer").classList.toggle("open")
+  })
   randomizeContent()
   showcaseSetup()
 }
